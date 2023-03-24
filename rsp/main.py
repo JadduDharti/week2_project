8fddc3502e364a7f588b3b544474f5a445f0174a
JadduDharti <dhartipatel89@gmail.com>
2023-03-24 11:04:11 -0500

    first commit

---
+++ rsp/main.py
@@ -0,0 +1,16 @@
+# Rock-Paper-Scissors Application
+
+#For this project, you and your partner(s) will work to create a program that has a 
+#"player" and a "computer" adversary. 
+#The computer should randomly choose its decision based on a 
+#list of actions it can take("Rock", "Paper" or "Scissors"). 
+#The player should then have a chance to input their decision.
+
+
+
+#Main Coding starts from here.
+
+#importing file for random pic
+
+import random
+
